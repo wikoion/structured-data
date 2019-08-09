47bf575f033e32f94e0ef0976b7a5caf19e4de91
Richard Draycott <richard.draycott@plus.net>
2019-08-09 12:27:36 +0100

    44 of 87 tests passed

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [sum (+ x x)]
+    (Math/pow sum sum)))
 
 (defn spiff [v]
-  :-)
+  (+ (v 0) (v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[a _ b] v]
+    (+ a b)))
 
 (defn point [x y]
   [x y])
@@ -19,19 +21,28 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (count (range x1 x2))))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (count (range y1 y2))))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (= (count (range x1 x2)) (count (range y1 y2)))
+      true
+      false)))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (count (range y1 y2)) (count (range x1 x2)))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 x2] [y1 y2]] rectangle [p1 p2] point]
+    (if (and (> p1 (count (range x1 x2))) (count (range y1 y2)))
+      true
+      false)))
 
 (defn contains-rectangle? [outer inner]
   :-)
